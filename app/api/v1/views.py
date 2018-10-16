c82696a878f6540f9ecc1f3a59aa5220e9e5a410
Dan Uluma <danuluma@gmail.com>
2018-10-16 23:30:46 +0300

    [Feature #161213606] added endpoint to post new books

--- app/api/v1/views.py
+++ app/api/v1/views.py
@@ -1,14 +1,20 @@
-from flask_restful import Resource
+from flask import request
+from flask_restful import Resource, reqparse
+from flask_jwt_extended import (
+    jwt_required, get_jwt_identity
+)
 
-products = [{
-        'id': 1,
-        'title': "Coming soon",
-        'description': "LOrem ipsum",
-        'price': 100,
-        'quantity': 20,
-        'minimun': 5,
-        'image_url':'coming_soon'
-    }]
+books = []
+
+
+parser = reqparse.RequestParser(bundle_errors=True)
+parser.add_argument('title', type=str, help='enter the book\'s name', location='json')
+parser.add_argument('description', type=str, location='json')
+parser.add_argument('price', type=int, help='price of the book', location='json')
+parser.add_argument('quantity', type=int, help='how many?', location='json')
+parser.add_argument('minimun', type=int, help='minimun required', location='json')
+parser.add_argument('image_url', type=str, help='url to the book\'s image', location='json')
+
 
 class Home(Resource):
   """Just a test endpoint ~/dann/api/v1/home"""
@@ -20,7 +26,28 @@ class Products(Resource):
   """docstring for Products"""
   """ endpoint for GET requests to /dann/api/v1/products"""
   def get(self):
-    if len(products) != 0:
-      return {"Books" : products}, 200
+    if len(books) != 0:
+      return {"Books" : books}, 200
     else:
       return {"Error": "There are no books"}, 404
+
+  """ endpoint for POST requests to /dann/api/v1/products"""
+  """ adds a new book"""
+  def post(self):
+    args = parser.parse_args()
+    book = [book for book in books if book['title'] == args['title']]
+    if len(book) != 0:
+      return {'Error':'Book already exists'}, 409
+    if args['title'].strip() == "":
+      return {'Error':'A book must have a valid title'}, 400
+    new_book = {
+        'id': len(books) + 1,
+        'title': args['title'],
+        'description': args['description'],
+        'price': args['price'],
+        'quantity': args['quantity'],
+        'minimun': args['minimun'],
+        'image_url': args['image_url']
+    }
+    books.append(new_book)
+    return {'message': 'Success! Book added'}, 200
--- app/tests/v1/test_v1.py
+++ app/tests/v1/test_v1.py
@@ -16,6 +16,15 @@ class Apiv1Test(unittest.TestCase):
   def setUp(self):
     self.app = create_app()
     self.client = self.app.test_client
+    self.test_book = {
+        'id': 1,
+        'title': "Coming soon",
+        'description': "LOrem ipsum",
+        'price': 100,
+        'quantity': 20,
+        'minimun': 5,
+        'image_url':'coming_soon'
+    }
 
   def tearDown(self):
     pass
@@ -30,12 +39,29 @@ class Apiv1Test(unittest.TestCase):
     self.assertEqual(response.status_code, 200)
 
   def test_get_empty_product_list(self):
-    """Tests /products endpoint. There are no items yet"""
+    """Tests GET /products endpoint. There are no items yet"""
     response = self.client().get('/dann/api/v1/products')
     json_data = json.loads(response.data)
     self.assertTrue(json_data.get('Error'))
     self.assertEqual(json_data.get('Error'), "There are no books")
     self.assertEqual(response.status_code, 404)
+
+  def test_add_new_book(self):
+    """Tests POST /products endpoint. Adds a new book"""
+    response = self.client().post('/dann/api/v1/products', json=self.test_book)
+    json_data = json.loads(response.data)
+    self.assertTrue(json_data.get('message'))
+    self.assertEqual(json_data.get('message'), "Success! Book added")
+    self.assertEqual(response.status_code, 200)
+
+  def test_get_all_books(self):
+    """Tests GET /products endpoint. There are no items yet"""
+    self.client().post('/dann/api/v1/products', json=self.test_book)
+    response = self.client().get('/dann/api/v1/products')
+    json_data = json.loads(response.data)
+    self.assertTrue(json_data.get('Books'))
+    self.assertEqual(response.status_code, 200)
+
 
 if __name__ == '__main__':
   unittest.main()
