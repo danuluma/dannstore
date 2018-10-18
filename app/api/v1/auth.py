e9d48733464b316e1defa82b35b35e2fd2466f2c
Dan Uluma <danuluma@gmail.com>
2018-10-19 00:55:24 +0300

    [Chore #161330034] added username&passw validation, updated tests

--- app/api/v1/auth.py
+++ app/api/v1/auth.py
@@ -2,6 +2,7 @@ from flask_restful import Resource, reqparse
 from flask_jwt_extended import (
     jwt_required, create_access_token, get_jwt_identity
 )
+import re
 
 
 users = [
@@ -9,10 +10,14 @@ users = [
 
 
 parser = reqparse.RequestParser(bundle_errors=True)
-parser.add_argument('username', type=str, required=True, help='please enter a username',  location='json')
-parser.add_argument('password', type=str, required=True, help='password can\'t be empty', location='json')
-parser.add_argument('role', type=int, help='enter user access level', location='json')
+parser.add_argument('username', type=str, required=True,
+                    help='please enter a username', location='json')
+parser.add_argument('password', type=str, required=True,
+                    help='password can\'t be empty', location='json')
+parser.add_argument(
+    'role', type=int, help='enter user access level', location='json')
 parser.add_argument('access_token', location='json')
+
 
 def find_user(username):
   """Finds and returns a user by username."""
@@ -21,24 +26,38 @@ def find_user(username):
   user = [user for user in users if user['username'] == username]
   return user
 
+
 def create_admin():
   """Creates the default admin account."""
 
   users.append({
-            "id": 0,
-            "username": "owner",
-            "password": "secret",
-            "role": 0
-        })
+      "id": 0,
+      "username": "owner",
+      "password": "secret1",
+      "role": 0
+  })
+
 
 def clear_users():
   """Clears the users list"""
 
   users.clear()
 
+
+def validate_password(passw):
+  """Validates password."""
+
+  return True if re.match("^(?=.*[A-Za-z])(?=.*[\d!@#$%&*?.])[A-Za-z\d!@#$%&*?.]{6,12}$", passw) else False
+
+
+def validate_username(username):
+  """Validates username"""
+
+  return True if re.match("^[A-Za-z\d]{4,10}$", username) else False
+
+
 class Register(Resource):
   """Maps to /reg endpoint"""
-
 
   @jwt_required
   def post(self):
@@ -51,17 +70,17 @@ class Register(Resource):
 
     user = find_user(username)
     if len(user) != 0:
-      return {'Error':'Username already exists'}, 409
-    if username == "":
-      return {'Error':'Please input a valid username'}, 400
-    if password == "":
-      return {'Error':'Please input a valid password'}, 400
+      return {'Error': 'Username already exists'}, 409
+    if validate_username(username) != True:
+      return {'Error': 'Please input a valid username'}, 400
+    if validate_password(password) != True:
+      return {'Error': 'Please input a valid password'}, 400
     new_user = {
-            'id': len(users) + 1,
-            'username': username,
-            'password': password,
-            'role': role
-            }
+        'id': len(users) + 1,
+        'username': username,
+        'password': password,
+        'role': role
+    }
     current_user = get_jwt_identity()
     if current_user[2] == 0:
       users.append(new_user)
@@ -81,15 +100,15 @@ class Login(Resource):
 
     user = find_user(username)
     if len(user) == 0:
-      return {'Error':'Username does not exist'}, 404
+      return {'Error': 'Username does not exist'}, 404
 
     if password != user[0]['password']:
-      return {'Error':'Wrong password'}, 401
+      return {'Error': 'Wrong password'}, 401
 
-    i_user = [user[0]['id'], user[0]['username'], user[0]['role']]
-    access_token = create_access_token(identity=i_user)
+    user_details = [user[0]['id'], user[0]['username'], user[0]['role']]
+    access_token = create_access_token(identity=user_details)
 
     mesg = {
         'access_token': access_token,
-        }
+    }
     return mesg, 200
